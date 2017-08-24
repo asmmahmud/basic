5117c02031146785e9f2d730554ebca1d7192fb2
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2017-08-24 14:54:31 +0600

    all edited

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 let div = document.createElement("div");
 div.className = "page-title";
-let h2 = document.querySelector("h2.tc");
-//let docFrag = document.createDocumentFragment();
-//docFrag.appendChild(h2);
+let h2 = document.querySelector("h1.tc");
+
 div.appendChild(h2);
-document.body.insertBefore(div, document.getElementById("script1"));
+document.body.insertBefore(div, document.body.firstElementChild);
